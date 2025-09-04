a3d4cd18cb01ab3781ddfa67f483d70dc20271e7
Fabiana Quelott <56900077+Fabianaquelott@users.noreply.github.com>
2025-09-04 15:26:20 -0300

    Fix metrics service fetch errors

--- src/core/utils/metrics-service.ts
+++ src/core/utils/metrics-service.ts
@@ -1,24 +1,30 @@
 // src/utils/metrics.ts
 export const trackPageView = async () => {
-  try {
-    await fetch('http://localhost:3001/track-page-view', { method: 'POST' })
-  } catch (err) {
-    console.error('Erro ao enviar métrica', err)
+  if (import.meta.env.PROD) {
+    try {
+      await fetch('http://localhost:3001/track-page-view', { method: 'POST' })
+    } catch (err) {
+      console.error('Erro ao enviar métrica', err)
+    }
   }
 }
 
 export const trackUserCreated = async () => {
-  try {
-    await fetch('http://localhost:3001/track-user-created', { method: 'POST' })
-  } catch (err) {
-    console.error('Erro ao enviar métrica', err)
+  if (import.meta.env.PROD) {
+    try {
+      await fetch('http://localhost:3001/track-user-created', { method: 'POST' })
+    } catch (err) {
+      console.error('Erro ao enviar métrica', err)
+    }
   }
 }
 
 export const trackStep2Success = async () => {
-  try {
-    await fetch('http://localhost:3001/track-step2-success', { method: 'POST' })
-  } catch (err) {
-    console.error('Erro ao enviar métrica', err)
+  if (import.meta.env.PROD) {
+    try {
+      await fetch('http://localhost:3001/track-step2-success', { method: 'POST' })
+    } catch (err) {
+      console.error('Erro ao enviar métrica', err)
+    }
   }
 }
